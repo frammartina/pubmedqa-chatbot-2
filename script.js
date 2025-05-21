b85eafd741cbe121a8e6d632b27faec266a77a25
Martina Frageri <154672533+frammartina@users.noreply.github.com>
2025-05-21 17:48:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -89,7 +89,7 @@ document.addEventListener("DOMContentLoaded", () => {
       
   
       try {
-        const response = await fetch("  https://4cbf-34-148-69-22.ngrok-free.app/chat", {
+        const response = await fetch(" https://f8e0-34-83-186-142.ngrok-free.app/chat", {
           method: "POST",
           headers: {
             "Content-Type": "application/json"
